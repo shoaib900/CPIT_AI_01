1b5078c6190ce9b04d133b3157886c8ecf4ffcfa
shoaib900 <rshoaib376@gmail.com>
2026-02-11 19:24:18 +0500

    uplaod simple code file

---
+++ index.py
@@ -0,0 +1,28 @@
+# print("hello world")
+# print(34)
+# name = 99
+# print(name)
+
+# FirstName # Pascal case writinge variable
+# firstName # camel case
+# first_name # snake case
+# primeMinister1st 
+
+# first$name = "hello"
+# print(first$name)
+# 1stname 
+
+# number = (12 + 5) * 2
+# print(number)
+
+# num1 = 5
+# num2 = 10
+# print(num1 * num2 + 10)
+
+# num = input("enter the name : ")
+# print(num)
+
+# n = "45"
+# print(n + type(n))
+
+
